d60221d31bb64ec29d11e7666fac0c23d94272b6
dakota-hawkins <dakotahawkins0110@gmail.com>
2021-10-08 10:04:37 -0400

    updated install script

--- workshops/git+snakemake/install.sh
+++ workshops/git+snakemake/install.sh
@@ -1,5 +1,5 @@
 module load miniconda
-conda create --name workshop seaborn scikit-learn statsmodels numba pytables
+conda create --name workshop seaborn scikit-learn statsmodels numba pytables pip
 conda activate workshop
-conda install -c conda-forge igraph python-igraph leidenalg imagemagick 
-pip install scanpy snakemake scikit-misc louvain jinja2 pygraphviz pygments
+conda install -c conda-forge igraph python-igraph leidenalg imagemagick louvain
+pip install scanpy==1.8.1 snakemake scikit-misc jinja2 pygraphviz pygments
