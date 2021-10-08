7c215d803bd9eb805769d82bdfb0abe70c6b48f8
dakota-hawkins <dakotahawkins0110@gmail.com>
2021-10-08 10:07:59 -0400

    explicit openpyxl install

--- workshops/git+snakemake/install.sh
+++ workshops/git+snakemake/install.sh
@@ -2,4 +2,4 @@ module load miniconda
 conda create --name workshop seaborn scikit-learn statsmodels numba pytables pip
 conda activate workshop
 conda install -c conda-forge igraph python-igraph leidenalg imagemagick louvain
-pip install scanpy==1.8.1 snakemake scikit-misc jinja2 pygraphviz pygments
+pip install scanpy==1.8.1 snakemake scikit-misc jinja2 pygraphviz pygments openpyxl
